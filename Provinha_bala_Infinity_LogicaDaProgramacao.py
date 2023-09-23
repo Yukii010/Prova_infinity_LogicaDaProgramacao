5c30fe35e2a04ba83929cbd6d0297840ffb93830
Lucas Cortes <125312920+Yukii010@users.noreply.github.com>
2023-09-23 14:10:28 -0300

    Add files via upload

---
+++ Provinha_bala_Infinity_LogicaDaProgramacao.py
@@ -0,0 +1,17 @@
+#Variável do tipo inteiro (int)
+idade = 18
+
+#Variável do tipo decimal (float)
+altura = 1.81
+
+#Variável do tipo string (str)
+nome = "Lucas"
+
+#Várivael do tipo lógico (bool)
+tem_cachorro = True
+
+#Segunda parte da prova
+nome_usuario = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+#Terceira parte da prova
+print("Olá,",nome_usuario,",Sua idade é: ",idade)
